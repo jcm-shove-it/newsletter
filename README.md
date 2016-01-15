e73f27c8fc026cd2cb15d8fd8e6724481f2880f4
Joerg Maier <jcm@shove-it.de>
2016-01-15 22:57:32 +0100

    email target and group changes ok, mail handling missing, tests in general missing

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Tool to send an email to a list of recipients, repeating the send process for ea
 
 -- add media aliase to apache, add wsgi to apache, enable mod_python
 -- create link to admin media contrib (css, ...), e.g. htdocs/media /usr/share/pyshared/django/contrib/admin/media
+
+TODOs:
+- lots if urls/views missing
+- need to split details (display, modify, create, delete?)
+- add check if user allowed to handle tagret/group/emailconto
--- listletter/views.py
+++ listletter/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 
 from django.contrib.auth.decorators import permission_required, login_required
 from django.template import RequestContext, Context, loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.core.exceptions import PermissionDenied
+
 
 from listletter.models import EmailTarget
 from listletter.models import EmailTargetGroup
@@ -38,15 +40,33 @@ def addressindex(request):
         )
     return HttpResponse(t.render(c))
 
-
+@permission_required('listletter.can_send')
+def groupindex(request):
+    all_groups = EmailTargetGroup.objects.filter(user__user__username=request.user.username)
+    return render_to_response('listletter/groupindex.html', {
+        'all_groups': all_groups,
+        'sidebar':True,
+    })
+    
 @permission_required('listletter.can_send')
 def addressdetail(request, contact_id):
-    isContacts=True
-    mode = ''
-    email = None
 
+    mode = 'initial'
+    email = None
+    emailTargetId = None
+
+    #############
+    # find mode #
+    #############
     try:
-        if request.POST['id'] == 'None':
+        emailTargetId = request.POST['id']
+    except Exception as e:
+        # no post data
+        mode = 'display'
+        emailTargetId = contact_id
+
+    if mode != 'display':
+        if emailTargetId == 'None':
             try:
                 if request.POST['_save_goto_contacts'] != None:
                     mode = 'create__goto_contacts'
@@ -55,36 +75,35 @@ def addressdetail(request, contact_id):
         else:
             try:
                 if request.POST['_save_goto_contacts'] != None:
-                    mode = 'change_goto_contacts'
+                    mode = 'change__goto_contacts'
             except Exception as e:
                 mode = 'change'
-                
-    except Exception as e:
-        # no post data
-        mode = 'display'
 
+
+    #######################################
+    # check if user is allowed to display #
+    #######################################
+    if int(emailTargetId) != 0:
+        if EmailTarget.objects.get(id=emailTargetId).user.user.username != request.user.username:
+            raise PermissionDenied
+
+    ###################
+    # execute request #
+    ###################
     if mode == 'display':
         try:
-            email = EmailTarget.objects.get(id=int(contact_id),user__user__username=request.user.username)
+            email = EmailTarget.objects.get(id=int(emailTargetId))
         except EmailTarget.DoesNotExist as e:
             email = EmailTarget()
     elif mode.startswith('create') or mode.startswith('change'):
         if mode.startswith('create'):
             email = EmailTarget()
         elif mode.startswith('change'):
-            email = EmailTarget.objects.get(id=request.POST['id'],user__user__username=request.user.username)
+            email = EmailTarget.objects.get(id=emailTargetId)
         email.name = request.POST['name']
         email.address = request.POST['address']
-        userName = request.user.username
-        llSender = ListletterSender.objects.filter(user__username=userName)
-        email.user = llSender[0]
-        #try:
-        #    a = request.POST['active']
-        #    if a == 'on':
-        #        email.active = True
-        #except:
-        #    email.active = False
         email.save()
+
         liste = request.POST.getlist('groups')
         email.groups.clear()
         for group_id in liste:
@@ -95,12 +114,138 @@ def addressdetail(request, contact_id):
     if mode.endswith('goto_contacts'):
         return render_to_response('listletter/addressindex.html', {
             'email_target_list': EmailTarget.objects.filter(user__user__username=request.user.username),
-            'isContacts': True,
             'sidebar':True,
             })
     else:
         return render_to_response('listletter/addressdetail.html', {
             'email': email,
             'groups': EmailTargetGroup.objects.filter(user__user__username=request.user.username),
-            'isContacts':isContacts
+            'mode': mode
             })
+
+@permission_required('listletter.can_send')
+def groupdetail(request, group_id):
+    mode = 'initial'
+    group = None
+    emailTargetGrouId = None
+
+    #############
+    # find mode #
+    #############
+    try:
+        emailTargetGrouId = request.POST['id']
+    except Exception as e:
+        # no post data
+        mode = 'display'
+        emailTargetGrouId = group_id
+
+    if mode != 'display':
+        if emailTargetGrouId == 'None':
+            try:
+                if request.POST['_save_goto_groups'] != None:
+                    mode = 'create_goto_groups'
+            except Exception as e:
+                mode = 'create'
+        else:
+            try:
+                if request.POST['_save_goto_groups'] != None:
+                    mode = 'change_goto_groups'
+            except Exception as e:
+                mode = 'change'
+    
+
+    #######################################
+    # check if user is allowed to display #
+    #######################################
+    if int(emailTargetGrouId) != 0:
+        if EmailTargetGroup.objects.get(id=emailTargetGrouId).user.user.username != request.user.username:
+            raise PermissionDenied
+
+    ###################
+    # execute request #
+    ###################
+    if mode == 'display':
+        try:
+            group = EmailTargetGroup.objects.get(id=emailTargetGrouId)
+        except EmailTargetGroup.DoesNotExist as e:
+            group = EmailTargetGroup()
+    elif mode.startswith('create') or mode.startswith('change'):
+        if mode.startswith('create'):
+            group = EmailTargetGroup()
+        elif mode.startswith('change'):
+            group = EmailTargetGroup.objects.get(id=emailTargetGrouId)
+        group.name = request.POST['name']
+        group.description = request.POST['description']
+        group.save()
+
+    if mode.endswith('goto_groups'):
+        return render_to_response('listletter/groupindex.html', {
+            'all_groups': EmailTargetGroup.objects.filter(user__user__username=request.user.username),
+            'sidebar':True,
+            })
+    else:
+        return render_to_response('listletter/addressgroupdetail.html', {
+            'group': group,
+            })
+
+@permission_required('listletter.can_send')
+def deleteaddress(request, contact_id):
+    email = EmailTarget.objects.get(id=contact_id)
+    email_name = email.name
+    
+    ######################################
+    # check if user is allowed to delete #
+    ######################################
+    if email.user.user.username != request.user.username:
+        raise PermissionDenied
+
+    try:
+        if request.POST['post'] == 'yes':
+            email.delete()
+            return render_to_response('listletter/deleteaddress.html', {
+                'email':None,
+                'email_name':email.name,
+                'email_deleted': 'True',
+                'isContacts':isContacts
+                })
+    except:
+        pass
+    
+    return render_to_response('listletter/deleteaddress.html', {
+        'email': email,
+        'email_name':email.name,
+        'email_deleted': 'False',
+        'isContacts':isContacts,
+        })
+           
+@permission_required('listletter.can_send')
+def deletegroup(request, group_id):
+    isContacts=True
+    group = EmailTargetGroup.objects.get(id=group_id)
+    group_name = group.name
+    
+    ######################################
+    # check if user is allowed to delete #
+    ######################################
+    if group.user.user.username != request.user.username:
+        raise PermissionDenied
+
+    try:
+        if request.POST['post'] == 'yes':
+            group.delete()
+            return render_to_response('listletter/deleteaddressgroup.html', {
+                'group':None,
+                'group_name':group.name,
+                'group_deleted': 'True',
+                'isContacts':isContacts,
+                })
+    except:
+        pass
+    
+    return render_to_response('listletter/deleteaddressgroup.html', {
+        'group': group,
+        'group_name':group.name,
+        'group_deleted': 'False',
+        'isContacts':isContacts
+        })
+
